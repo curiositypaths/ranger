4a07751a57c0c50aacff0a4236e6058f6fc9e1f0
Jason Decker <curiositypaths@users.noreply.github.com>
2024-02-13 09:51:27 -0500

    Testing JS Hooks

--- test/ranger_web/live/directory_live_test.exs
+++ test/ranger_web/live/directory_live_test.exs
@@ -4,4 +4,34 @@ defmodule RangerWeb.DirectoryLiveTest do
   import Phoenix.LiveViewTest
 
   alias Ranger.Directory
+
+  test "user id directed to member information", %{conn: conn} do
+    member_name = "Frodo"
+    member = Directory.get_by(name: member_name)
+    {:ok, view, _html} = live(conn, ~p"/directory")
+
+    view |> element("[data-role=member]", member_name) |> render_click()
+
+    assert_patch(view, ~p"/directory/#{member.id}")
+  end
+
+  test "user can see member information when selected", %{conn: conn} do
+    member_name = "Frodo"
+    member = Directory.get_by(name: member_name)
+    {:ok, view, _html} = live(conn, ~p"/directory")
+
+    view |> element("[data-role=member]", member_name) |> render_click()
+
+    # The below would test implementation detail (i.e., assumes patch)
+    assert_patch(view, ~p"/directory/#{member.id}")
+    assert has_element?(view, "#active-member", member_name)
+  end
+
+  test "can visit specific member", %{conn: conn} do
+    member_name = "Frodo"
+    member = Directory.get_by(name: member_name)
+    {:ok, view, _html} = live(conn, ~p"/directory/#{member.id}")
+
+    assert has_element?(view, "#active-member", member_name)
+  end
 end
