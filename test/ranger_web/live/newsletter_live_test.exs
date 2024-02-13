803e133eaa3a20a6825fc59e53528625fb5a9063
Jason Decker <curiositypaths@users.noreply.github.com>
2024-02-12 21:29:31 -0500

    Testing Form Changes

--- test/ranger_web/live/newsletter_live_test.exs
+++ test/ranger_web/live/newsletter_live_test.exs
@@ -2,4 +2,33 @@ defmodule RangerWeb.NewsletterLiveTest do
   use RangerWeb.ConnCase
 
   import Phoenix.LiveViewTest
+
+  test "warns user of invalid email as user types", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/newsletter")
+
+    invalid_email = "not an email address"
+
+    html =
+      view
+      |> form("#subscribe", %{subscription: %{email: invalid_email}})
+      |> render_change()
+
+    # ok to use html since the string "has invalid format" is unlikely
+    # to be present otherwide on the page
+    assert html =~ "has invalid format"
+  end
+
+  test "warns user of invalid email as user types (using event)", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/newsletter")
+
+    invalid_email = "not an email address"
+
+    html =
+      view
+      |> render_change("validate", %{subscription: %{email: invalid_email}})
+
+    # ok to use html since the string "has invalid format" is unlikely
+    # to be present otherwide on the page
+    assert html =~ "has invalid format"
+  end
 end
